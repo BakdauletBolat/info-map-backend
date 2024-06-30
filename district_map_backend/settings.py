ef3779f260ec09838eb8147464e7b94c5f849903
Bakdaulet <bakosh21345@gmail.com>
2024-06-30 20:01:57 +0500

    added allowed hosts

--- district_map_backend/settings.py
+++ district_map_backend/settings.py
@@ -87,13 +87,6 @@ WSGI_APPLICATION = 'district_map_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
