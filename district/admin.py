a46614fea6e03f4d37161eae4111669be66d5bbf
Bakdaulet <bakosh21345@gmail.com>
2024-07-15 23:29:17 +0500

    added select

--- district/admin.py
+++ district/admin.py
@@ -11,6 +11,8 @@ class GeographicRegionTabularAdmin(admin.TabularInline):
 class GeographicRegionAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "dwelling_count", "population_count")
     list_display_links = ("id", "name")
+    search_fields = ("name", )
+    autocomplete_fields = ("parent", )
     # exclude = ('slug',)
     prepopulated_fields = {'slug': ('name',)}
     inlines = [GeographicRegionTabularAdmin]
