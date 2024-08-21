32df67fafa45be998690d6fdebdfc2f2cc1d6f9d
Bakdaulet <bakosh21345@gmail.com>
2024-08-21 23:06:20 +0500

    adding new api and authorization

--- auth_user/views.py
+++ auth_user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.authtoken.models import Token
+from rest_framework import decorators
 
-# Create your views here.
+
+@decorators.action(methods=["GET"])
+def get_user(request):
+    token_header = request.headers.get("Authorization")
+    token = token_header.split(" ")[1]
+    token_model = Token.objects.get(key=token)
+    return {
+        "user": token_model.user.username,
+        "id": token_model.user.id
+    }
--- district_map_backend/urls.py
+++ district_map_backend/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken import views
+from auth_user import views as auth_views
 
 urlpatterns = [
     path('api/', include('district.urls')),
     path('api/', include('geometry.urls')),
     path('auth/token/', views.obtain_auth_token),
+    path('auth/me/', auth_views.get_user),
     path('admin/', admin.site.urls),
 ]
 
