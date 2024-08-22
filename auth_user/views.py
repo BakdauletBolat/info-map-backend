613b25937dbb3d7c1850ebbf258f28c27cc84521
Bakdaulet <bakosh21345@gmail.com>
2024-08-22 21:35:31 +0500

    adding new api and authorization

--- auth_user/views.py
+++ auth_user/views.py
@@ -2,7 +2,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework import decorators
 
 
-@decorators.action(methods=["GET"])
+@decorators.action(methods=["GET"], detail=False)
 def get_user(request):
     token_header = request.headers.get("Authorization")
     token = token_header.split(" ")[1]
